ff588d67bed31316164f1a7482d558897aff4fd2
namlunkaka <namlun.kaka.12@gmail.com>
2020-03-23 20:48:44 +0700

    First commit

---
+++ src/config/db.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+import 'firebase/database'
+
+const config = {
+    apiKey: "AIzaSyAUwpj1XqHMxmbBHn8y04jo95JIDZRVajo",
+    authDomain: "bep-nha-moc.firebaseapp.com",
+    databaseURL: "https://bep-nha-moc.firebaseio.com",
+    projectId: "bep-nha-moc",
+    storageBucket: "bep-nha-moc.appspot.com",
+    messagingSenderId: "874438072549",
+    appId: "1:874438072549:web:dc6cfda26ca6a81e155c0c",
+    measurementId: "G-XPGQV237T0"
+  };
+
+// firebase utils
+export const db = firebase
+  .initializeApp(config)
+  .database();
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-alert show>{{ msg }}</b-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  },
+  data () {
+    return {
+      msg: 'Welcome to Bếp nhà Mộc'
+    }
+  }
+}
+</script>
